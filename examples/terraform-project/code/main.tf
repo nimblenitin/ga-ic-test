cc0936df195dd2302fea3062882d2d96ccb50717
nimblenitin <naidunitin1@gmail.com>
2022-08-09 19:25:38 +0530

    feat: adding vpc

--- examples/terraform-project/code/main.tf
+++ examples/terraform-project/code/main.tf
@@ -35,20 +35,37 @@ module "vpc" {
   tags = var.tags
 }
 
-# creates cloudwatch log group for log stream
+# Creates VPC
 
-resource "aws_cloudwatch_log_group" "log" {
-  name_prefix       = var.name
-  retention_in_days = var.cloudwatch_log_retention
-  tags              = var.tags
+data "aws_availability_zones" "zones" {
 }
 
-# creates cloudwatch log stream from a particular source
+# https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = ">=3.14.0"
+  name = "${var.name}-vpc"
+  
+  cidr = "10.0.0.0/16"
 
-resource "aws_cloudwatch_log_stream" "stream" {
-  name           = "${var.name}-alerts"
-  log_group_name = aws_cloudwatch_log_group.log.name
+  private_subnets         = ["10.0.1.0/24"]
+  public_subnets          = ["10.0.101.0/24"]
+  map_public_ip_on_launch = false
+
+  azs = length(var.ecs_vpc_region_azs) > 0 ? var.ecs_vpc_region_azs : [
+    data.aws_availability_zones.zones.names[0],
+    data.aws_availability_zones.zones.names[1]
+  ]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  enable_nat_gateway   = true
+  enable_vpn_gateway   = false
+
+  tags = var.tags
 }
+
+
 
 
 

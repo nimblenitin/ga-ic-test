6f6ba76b3cfe4eb9e0da33d6d083981f0102c66d
nimblenitin <naidunitin1@gmail.com>
2022-08-09 18:30:22 +0530

    feat: adding ecs and vpc

---
+++ variables.tf
@@ -0,0 +1,23 @@
+# general
+
+variable "name" {
+  type        = string
+  description = "Prefix name for all resources"
+  default = "test"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Default tag for resource"
+  default = {
+    "product" = "test"
+  }
+}
+
+# vpc 
+
+variable "ecs_vpc_region_azs" {
+  type        = list(string)
+  description = "List of Availability Zones for ECS VPC creation. e.g.: [\"apne1-az1\", \"apne1-az2\"]. If defaulted, two of the default 'aws_availability_zones' datasource will be taken"
+  default     = []
+}
